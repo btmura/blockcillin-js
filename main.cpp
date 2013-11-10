a75f84d975c6e86e348d0f1dae92eda531559198
Brian Muramatsu <btmura@gmail.com>
2013-11-09 21:45:47 -0800

    add event loop and close window on SDL_QUIT

--- main.cpp
+++ main.cpp
@@ -27,13 +27,22 @@ int main(int argc, char *argv[]) {
 	}
 
 	SDL_Surface *surface = SDL_GetWindowSurface(window);
-	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0, 0));
-	SDL_UpdateWindowSurface(window);
+	SDL_Event event;
+	bool quit = false;
 
-	SDL_Delay(5000);
+	while (!quit) {
+		while (SDL_PollEvent(&event) != 0) {
+			switch (event.type) {
+				case SDL_QUIT:
+					quit = true;
+					break;
+			}
+		}
+		SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0, 0));
+		SDL_UpdateWindowSurface(window);
+	}
 
 	SDL_DestroyWindow(window);
-
 	SDL_Quit();
 	return 0;
 }
