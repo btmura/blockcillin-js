e6e3bef8bf538137538972f3c7c47cb1494ce315
Brian Muramatsu <btmura@gmail.com>
2013-11-09 20:59:47 -0800

    remove using namespace std

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-
 #include "SDL/SDL.h"
-
-using namespace std;
 
 int main(int argc, char *argv[]) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		cerr << "SDL_Init failed: " << SDL_GetError() << endl;
+		std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
 		return 1;
 	}
 
-	cout << "blockcillin" << endl;
+	std::cout << "blockcillin" << std::endl;
 
 	SDL_Quit();
 	return 0;
