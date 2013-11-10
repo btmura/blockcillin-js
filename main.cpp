0d4072b188d02b6d66abdbb122d40203f9670a31
Brian Muramatsu <btmura@gmail.com>
2013-11-09 21:14:12 -0800

    display a blank window for 5 seconds

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-#include "SDL/SDL.h"
+#include "SDL2/SDL.h"
+
+const std::string WindowTitle = "blockcillin";
+const int WindowX = SDL_WINDOWPOS_UNDEFINED;
+const int WindowY = SDL_WINDOWPOS_UNDEFINED;
+const int WindowWidth = 640;
+const int WindowHeight = 480;
+const int WindowFlags = SDL_WINDOW_OPENGL;
 
 int main(int argc, char *argv[]) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -7,7 +14,21 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	std::cout << "blockcillin" << std::endl;
+	SDL_Window *window = SDL_CreateWindow(
+		WindowTitle.c_str(),
+		WindowX,
+		WindowY,
+		WindowWidth,
+		WindowHeight,
+		WindowFlags);
+	if (window == NULL) {
+		std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_Delay(5000);
+
+	SDL_DestroyWindow(window);
 
 	SDL_Quit();
 	return 0;
