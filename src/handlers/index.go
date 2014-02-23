43c5085c7efe68ba3d11374f447f240edd232b88
Brian Muramatsu <btmura@gmail.com>
2014-02-23 13:40:47 -0800

    use single template for compiled and not compiled

--- src/handlers/index.go
+++ src/handlers/index.go
@@ -8,6 +8,16 @@ import (
 
 var indexTemplate = template.MustParseFiles("templates/index.html")
 
+type IndexTemplateArgs struct {
+	Compiled bool
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	indexTemplate.Execute(w, nil)
+	args := &IndexTemplateArgs{Compiled: true}
+	indexTemplate.Execute(w, args)
+}
+
+func debugHandler(w http.ResponseWriter, r *http.Request) {
+	args := &IndexTemplateArgs{Compiled: false}
+	indexTemplate.Execute(w, args)
 }
