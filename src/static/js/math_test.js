571dd642ceda52b4226c178fdbd857213dc04c8b
Brian Muramatsu <btmura@gmail.com>
2014-03-07 22:25:08 -0800

    add time tests to test running multiple tests

--- src/static/js/math_test.js
+++ src/static/js/math_test.js
@@ -1,4 +1,4 @@
-test("radians", function() {
+test("BC.Math.radians", function() {
 	equal(BC.Math.radians(0), 0, "0 degrees");
 	equal(BC.Math.radians(90), Math.PI / 2, "90 degrees");
 	equal(BC.Math.radians(180), Math.PI, "180 degrees");
---
+++ src/static/js/time_test.js
@@ -0,0 +1,14 @@
+test("BC.Time.getTimeInSeconds", function() {
+
+	function setTime(ms) {
+		Date.now = function() {
+			return ms;
+		};
+	}
+
+	setTime(1000);
+	equal(BC.Time.getTimeInSeconds(), 1, "1 second");
+
+	setTime(2500);
+	equal(BC.Time.getTimeInSeconds(), 2.5, "2.5 seconds");
+});
