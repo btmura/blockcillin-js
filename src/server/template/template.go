7939d22a17d7419ff944c03a6a39a93a660b5e3b
Brian Muramatsu <btmura@gmail.com>
2014-03-08 10:42:06 -0800

    clean up template utils

--- src/server/template/template.go
+++ src/server/template/template.go
@@ -5,28 +5,31 @@ import (
 	"net/http"
 )
 
-var indexTemplate = MustParseFiles(
+var indexTemplate = mustParseFiles(
 	"client/templates/index.html",
 	"client/templates/common.html")
 
-var testsTemplate = MustParseFiles(
+var testsTemplate = mustParseFiles(
 	"client/templates/tests.html",
 	"client/templates/common.html")
 
 // MustParseFiles creates a template out of the filenames or panics.
-func MustParseFiles(filenames ...string) *template.Template {
+func mustParseFiles(filenames ...string) *template.Template {
 	return template.Must(template.ParseFiles(filenames...))
 }
 
+// IndexArgs are arguments passed to the index template.
 type IndexArgs struct {
 	Compiled bool
 	Debug    bool
 }
 
+// ExecuteIndex executes the index template which runs the game.
 func ExecuteIndex(w http.ResponseWriter, args *IndexArgs) {
 	indexTemplate.Execute(w, args)
 }
 
+// ExecuteTests executes the tests template which runs the unit tests.
 func ExecuteTests(w http.ResponseWriter) {
 	testsTemplate.Execute(w, nil)
 }
