86032020e87c5c49ecf9fe936b933c4f8aff153f
Brian Muramatsu <btmura@gmail.com>
2014-03-08 11:16:09 -0800

    remove debug handler

--- src/server/handlers/init.go
+++ src/server/handlers/init.go
@@ -8,17 +8,11 @@ import (
 
 func init() {
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/debug", debugHandler)
 	http.HandleFunc("/tests", testsHandler)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	args := &template.IndexArgs{Compiled: true, ShowTests: false}
-	template.ExecuteIndex(w, args)
-}
-
-func debugHandler(w http.ResponseWriter, r *http.Request) {
-	args := &template.IndexArgs{Compiled: false, ShowTests: true}
 	template.ExecuteIndex(w, args)
 }
 
