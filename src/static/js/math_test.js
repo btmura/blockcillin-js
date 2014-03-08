d3ce7e3f660f6fd88a2ac74b7cad2e591a89a822
Brian Muramatsu <btmura@gmail.com>
2014-03-07 22:08:58 -0800

    refactor tests with sample math tests

---
+++ src/static/js/math_test.js
@@ -0,0 +1,8 @@
+test("radians", function() {
+	equal(BC.Math.radians(0), 0, "0 degrees");
+	equal(BC.Math.radians(90), Math.PI / 2, "90 degrees");
+	equal(BC.Math.radians(180), Math.PI, "180 degrees");
+	equal(BC.Math.radians(360), 2 * Math.PI, "360 degrees");
+});
+
+
