3691315f63fe5a4e1494bb83cabecdfed683a687
Brian Muramatsu <btmura@gmail.com>
2014-03-08 10:25:35 -0800

    consolidate small handler files into one

--- src/server/handlers/init.go
+++ src/server/handlers/init.go
@@ -2,10 +2,26 @@ package handlers
 
 import (
 	"net/http"
+
+	"server/template"
 )
 
 func init() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/debug", debugHandler)
 	http.HandleFunc("/tests", testsHandler)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	args := &template.IndexArgs{Compiled: true, Debug: false}
+	template.ExecuteIndex(w, args)
+}
+
+func debugHandler(w http.ResponseWriter, r *http.Request) {
+	args := &template.IndexArgs{Compiled: false, Debug: true}
+	template.ExecuteIndex(w, args)
+}
+
+func testsHandler(w http.ResponseWriter, r *http.Request) {
+	template.ExecuteTests(w);
 }
