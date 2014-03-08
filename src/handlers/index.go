60a1557aafd701d004dbab42ea2f9b34b4b33fbe
Brian Muramatsu <btmura@gmail.com>
2014-03-07 22:43:14 -0800

    add tests menu item at the top

--- src/handlers/index.go
+++ src/handlers/index.go
@@ -10,14 +10,15 @@ var indexTemplate = template.MustParseFiles("templates/index.html")
 
 type IndexTemplateArgs struct {
 	Compiled bool
+	Debug    bool
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	args := &IndexTemplateArgs{Compiled: true}
+	args := &IndexTemplateArgs{Compiled: true, Debug: false}
 	indexTemplate.Execute(w, args)
 }
 
 func debugHandler(w http.ResponseWriter, r *http.Request) {
-	args := &IndexTemplateArgs{Compiled: false}
+	args := &IndexTemplateArgs{Compiled: false, Debug: true}
 	indexTemplate.Execute(w, args)
 }
