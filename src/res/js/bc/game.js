05fc72edb7f3f459496a59b6e4130ea4a08c8566
Brian Muramatsu <btmura@gmail.com>
2014-04-07 21:00:16 -0700

    add texture filtering parameters

--- src/res/js/bc/game.js
+++ src/res/js/bc/game.js
@@ -26,6 +26,8 @@ var BC = (function(parent) {
 		$(image).load(function() {
 			gl.bindTexture(gl.TEXTURE_2D, texture);
 			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
 			gl.generateMipmap(gl.TEXTURE_2D);
 		});
 
