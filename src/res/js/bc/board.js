ee1d7dd572d797e4a30394d2f6b4091a6c46a1b8
Brian Muramatsu <btmura@gmail.com>
2014-04-10 19:07:16 -0700

    move selector code out of the board module

--- src/res/js/bc/board.js
+++ src/res/js/bc/board.js
@@ -19,7 +19,6 @@ var BC = (function(parent) {
 		};
 
 		var maxCellSwapTime = 0.125;
-		var maxSelectorMovementTime = 0.05;
 
 		var ringRotationY = 2 * Math.PI / specs.numRingCells;
 		var ringTranslationY = specs.ringMaxY - specs.ringMinY;
@@ -62,14 +61,7 @@ var BC = (function(parent) {
 			};
 		}
 
-		var selector = {
-			matrix: BC.Matrix.identity,
-
-			// private details
-			direction: Direction.NONE,
-			elapsedMovementTime: 0,
-			translation: [0, 0, 0]
-		};
+		var selector = BC.Selector.make();
 
 		var board = {
 			rings: rings,
@@ -92,7 +84,6 @@ var BC = (function(parent) {
 			rotation: [0, 0, 0]
 		};
 
-
 		function move(direction) {
 			switch (direction) {
 				case Direction.LEFT:
@@ -114,9 +105,7 @@ var BC = (function(parent) {
 		}
 
 		function moveSelectorLeft() {
-			if (selector.direction === Direction.NONE) {
-				selector.direction = Direction.LEFT;
-				selector.elapsedMovementTime = 0;
+			if (selector.move(Direction.LEFT)) {;
 				board.currentCell--;
 				if (board.currentCell < 0) {
 					board.currentCell = specs.numRingCells - 1;
@@ -125,9 +114,7 @@ var BC = (function(parent) {
 		}
 
 		function moveSelectorRight() {
-			if (selector.direction === Direction.NONE) {
-				selector.direction = Direction.RIGHT;
-				selector.elapsedMovementTime = 0;
+			if (selector.move(Direction.RIGHT)) {
 				board.currentCell++;
 				if (board.currentCell >= specs.numRingCells) {
 					board.currentCell = 0;
@@ -136,17 +123,13 @@ var BC = (function(parent) {
 		}
 
 		function moveSelectorUp() {
-			if (selector.direction === Direction.NONE && board.currentRing > 0) {
-				selector.direction = Direction.UP;
-				selector.elapsedMovementTime = 0;
+			if (board.currentRing > 0 && selector.move(Direction.UP)) {
 				board.currentRing--;
 			}
 		}
 
 		function moveSelectorDown() {
-			if (selector.direction === Direction.NONE && board.currentRing + 1 < board.rings.length) {
-				selector.direction = Direction.DOWN;
-				selector.elapsedMovementTime = 0;
+			if (board.currentRing + 1 < board.rings.length && selector.move(Direction.DOWN)) {
 				board.currentRing++;
 			}
 		}
@@ -198,41 +181,9 @@ var BC = (function(parent) {
 				}
 			}
 
-			if (selector.direction !== Direction.NONE) {
-				if (selector.elapsedMovementTime + deltaTime > maxSelectorMovementTime) {
-					deltaTime = maxSelectorMovementTime - selector.elapsedMovementTime;
-				}
-
-				var translationDelta = deltaTime * ringTranslationY / maxSelectorMovementTime;
-				var rotationDelta = deltaTime * ringRotationY / maxSelectorMovementTime;
-
-				switch (selector.direction) {
-					case Direction.UP:
-						selector.translation[1] += translationDelta;
-						break;
-
-					case Direction.DOWN:
-						selector.translation[1] -= translationDelta;
-						break;
-
-					case Direction.LEFT:
-						board.rotation[1] += rotationDelta;
-						break;
-
-					case Direction.RIGHT:
-						board.rotation[1] -= rotationDelta;
-						break;
-				}
-
-				selector.elapsedMovementTime += deltaTime;
-				if (selector.elapsedMovementTime >= maxSelectorMovementTime) {
-					selector.direction = Direction.NONE;
-					selector.elapsedMovementTime = 0;
-				}
-			}
-
 			updateBoardMatrix();
-			updateSelectorMatrix(now);
+
+			selector.update(deltaTime, now, ringTranslationY, ringRotationY, board.rotation);
 		}
 
 		function updateBoardMatrix() {
@@ -243,16 +194,6 @@ var BC = (function(parent) {
 			var matrix = BC.Matrix.matrixMultiply(rotationZMatrix, rotationYMatrix);
 			matrix = BC.Matrix.matrixMultiply(matrix, rotationXMatrix);
 			board.matrix = matrix;
-		}
-
-		function updateSelectorMatrix(now) {
-			var scale = 1 + Math.abs(Math.sin(4 * now)) / 25;
-			var scaleMatrix = BC.Matrix.makeScale(scale, scale, 1);
-			var translationMatrix = BC.Matrix.makeTranslation(
-					selector.translation[0],
-					selector.translation[1],
-					selector.translation[2]);
-			board.selector.matrix = BC.Matrix.matrixMultiply(scaleMatrix, translationMatrix);
 		}
 
 		return board;
--- src/res/js/bc/selector.js
+++ src/res/js/bc/selector.js
@@ -2,6 +2,122 @@ var BC = (function(parent) {
 
 	var my = parent.Selector = parent.Selector || {}
 
+	my.make = function() {
+		var Direction = BC.Constants.Direction;
+
+		var maxSelectorMovementTime = 0.05;
+
+		var selector = {
+			matrix: BC.Matrix.identity,
+
+			move: move,
+			update: update,
+
+			// private details
+			direction: Direction.NONE,
+			elapsedMovementTime: 0,
+			translation: [0, 0, 0]
+		};
+
+		function move(direction) {
+			switch (direction) {
+				case Direction.LEFT:
+					return moveLeft();
+
+				case Direction.RIGHT:
+					return moveRight();
+
+				case Direction.UP:
+					return moveUp();
+
+				case Direction.DOWN:
+					return moveDown();
+			}
+		}
+
+		function moveLeft() {
+			if (selector.direction === Direction.NONE) {
+				startMoving(Direction.LEFT);
+				return true;
+			}
+			return false;
+		}
+
+		function moveRight() {
+			if (selector.direction === Direction.NONE) {
+				startMoving(Direction.RIGHT);
+				return true;
+			}
+			return false;
+		}
+
+		function moveUp() {
+			if (selector.direction === Direction.NONE) {
+				startMoving(Direction.UP);
+				return true;
+			}
+			return false;
+		}
+
+		function moveDown() {
+			if (selector.direction === Direction.NONE) {
+				startMoving(Direction.DOWN);
+				return true;
+			}
+			return false;
+		}
+
+		function startMoving(direction) {
+			selector.direction = direction;
+			selector.elapsedMovementTime = 0;
+		}
+
+		function update(deltaTime, now, ringTranslationY, ringRotationY, boardRotation) {
+			if (selector.direction !== Direction.NONE) {
+				if (selector.elapsedMovementTime + deltaTime > maxSelectorMovementTime) {
+					deltaTime = maxSelectorMovementTime - selector.elapsedMovementTime;
+				}
+
+				var translationDelta = deltaTime * ringTranslationY / maxSelectorMovementTime;
+				var rotationDelta = deltaTime * ringRotationY / maxSelectorMovementTime;
+
+				switch (selector.direction) {
+					case Direction.UP:
+						selector.translation[1] += translationDelta;
+						break;
+
+					case Direction.DOWN:
+						selector.translation[1] -= translationDelta;
+						break;
+
+					case Direction.LEFT:
+						boardRotation[1] += rotationDelta;
+						break;
+
+					case Direction.RIGHT:
+						boardRotation[1] -= rotationDelta;
+						break;
+				}
+
+				selector.elapsedMovementTime += deltaTime;
+				if (selector.elapsedMovementTime >= maxSelectorMovementTime) {
+					selector.direction = Direction.NONE;
+					selector.elapsedMovementTime = 0;
+				}
+			}
+
+			var scale = 1 + Math.abs(Math.sin(4 * now)) / 25;
+			var scaleMatrix = BC.Matrix.makeScale(scale, scale, 1);
+			var translationMatrix = BC.Matrix.makeTranslation(
+					selector.translation[0],
+					selector.translation[1],
+					selector.translation[2]);
+			selector.matrix = BC.Matrix.matrixMultiply(scaleMatrix, translationMatrix);
+		}
+
+		return selector;
+	};
+
 	return parent;
 
 }(BC || {}))
