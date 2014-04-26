2952354f83e8fa4efcd3676d5cac0d63ca68b88b
Brian Muramatsu <btmura@gmail.com>
2014-04-25 22:46:01 -0700

    add vertical chain detection with test

--- src/res/js/bc-tests/chain_test.js
+++ src/res/js/bc-tests/chain_test.js
@@ -1,1 +1,95 @@
 module("BC.Chain");
+
+test("find_verticalChain_three", function() {
+	var board = BC.Chain.makeBoard([
+		[1],
+		[1],
+		[1],
+		[0]
+	]);
+
+	var actual = BC.Chain.find(board);
+	var expected = [
+		[
+			{
+				cell: board.rings[0].cells[0],
+				row: 0,
+				col: 0
+			},
+			{
+				cell: board.rings[1].cells[0],
+				row: 1,
+				col: 0
+			},
+			{
+				cell: board.rings[2].cells[0],
+				row: 2,
+				col: 0
+			}
+		]
+	];
+
+	deepEqual(actual, expected);
+});
+
+test("find_verticalChain_four", function() {
+	var board = BC.Chain.makeBoard([
+		[1],
+		[1],
+		[1],
+		[1]
+	]);
+
+	var actual = BC.Chain.find(board);
+	var expected = [
+		[
+			{
+				cell: board.rings[0].cells[0],
+				row: 0,
+				col: 0
+			},
+			{
+				cell: board.rings[1].cells[0],
+				row: 1,
+				col: 0
+
+			},
+			{
+				cell: board.rings[2].cells[0],
+				row: 2,
+				col: 0
+
+			},
+			{
+				cell: board.rings[3].cells[0],
+				row: 3,
+				col: 0
+			}
+		]
+	];
+
+	deepEqual(actual, expected);
+});
+
+QUnit.extend(BC.Chain, {
+	makeBoard: function(specs) {
+		var rings = [];
+		for (var i = 0; i < specs.length; i++) {
+			var cells = [];
+			for (var j = 0; j < specs[i].length; j++) {
+				var cell = {
+					blockStyle: specs[i][j]
+				};
+				cells.push(cell);
+			}
+			var ring = {
+				cells: cells
+			};
+			rings.push(ring);
+		}
+
+		return {
+			rings: rings
+		};
+	}
+});
--- src/res/js/bc/chain.js
+++ src/res/js/bc/chain.js
@@ -2,8 +2,47 @@ var BC = (function(parent) {
 
 	var my = parent.Chain = parent.Chain || {}
 
-	my.make = function() {
+	my.find = function(board) {
+		var chains = [];
 
+		function getCell(row, col) {
+			return board.rings[row].cells[col];
+		}
+
+		var numRows = 4;
+		var col = 0;
+
+		// Find vertical chains of 3+ cells
+		for (var startRow = 0; startRow < numRows; ) {
+			var cell = getCell(startRow, col);
+			var blockStyle = cell.blockStyle;
+
+			for (var endRow = startRow + 1; endRow < numRows; endRow++) {
+				var cell2 = getCell(endRow, col);
+				var blockStyle2 = cell2.blockStyle;
+				if (blockStyle !== blockStyle2) {
+					break;
+				}
+			}
+
+			var matching = endRow - startRow;
+			if (matching >= 3) {
+				var newChain = [];
+				for (var matchRow = startRow; matchRow < endRow; matchRow++) {
+					var matchCell = getCell(matchRow, col);
+					newChain.push({
+						cell: matchCell,
+						row: matchRow,
+						col: col
+					});
+				}
+				chains.push(newChain);
+			}
+
+			startRow = endRow;
+		}
+
+		return chains;
 	};
 
 	return parent;
