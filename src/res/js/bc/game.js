d4390025d3bf110dcfa3a235752b50c8b49366f7
Brian Muramatsu <btmura@gmail.com>
2014-05-16 17:43:49 -0700

    add ugly game menu with new game button

--- src/res/js/bc/game.js
+++ src/res/js/bc/game.js
@@ -5,164 +5,172 @@ var BC = (function(parent) {
 	my.run = function() {
 		var Direction = BC.Constants.Direction;
 
-		var canvas = document.getElementById("canvas");
-		if (!canvas) {
-			return;
-		}
+		var newGameButton = $("#new-game-button");
+		newGameButton.click(function() {
+			newGameButton.hide();
+			startGame();
+		});
 
-		var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-		if (!gl) {
-			return;
-		}
+		function startGame() {
+			var canvas = document.getElementById("canvas");
+			if (!canvas) {
+				return;
+			}
 
-		// Set the 1 required texture to a 1x1 placeholder and then fire off
-		// an async call to load the image while we do other things.
-		var texture = gl.createTexture();
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 0, 255]));
+			var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+			if (!gl) {
+				return;
+			}
 
-		var image = new Image();
-		image.src = "images/textures.png";
-		$(image).load(function() {
+			// Set the 1 required texture to a 1x1 placeholder and then fire off
+			// an async call to load the image while we do other things.
+			var texture = gl.createTexture();
 			gl.bindTexture(gl.TEXTURE_2D, texture);
-			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-			gl.generateMipmap(gl.TEXTURE_2D);
-		});
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 0, 255]));
 
-		gl.enable(gl.CULL_FACE);
-		gl.enable(gl.DEPTH_TEST);
+			var image = new Image();
+			image.src = "images/textures.png";
+			$(image).load(function() {
+				gl.bindTexture(gl.TEXTURE_2D, texture);
+				gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, image);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+				gl.generateMipmap(gl.TEXTURE_2D);
+			});
 
-		gl.enable(gl.BLEND);
-		gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+			gl.enable(gl.CULL_FACE);
+			gl.enable(gl.DEPTH_TEST);
 
-		var vertexShader = BC.GL.loadShader(gl, 'vertex-shader', gl.VERTEX_SHADER);
-		var fragmentShader = BC.GL.loadShader(gl, 'fragment-shader', gl.FRAGMENT_SHADER);
-		var program = BC.GL.createProgram(gl, [vertexShader, fragmentShader]);
-		gl.useProgram(program);
+			gl.enable(gl.BLEND);
+			gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
-		function getAttrib(name) {
-			return gl.getAttribLocation(program, name);
-		}
+			var vertexShader = BC.GL.loadShader(gl, 'vertex-shader', gl.VERTEX_SHADER);
+			var fragmentShader = BC.GL.loadShader(gl, 'fragment-shader', gl.FRAGMENT_SHADER);
+			var program = BC.GL.createProgram(gl, [vertexShader, fragmentShader]);
+			gl.useProgram(program);
 
-		function getUniform(name) {
-			return gl.getUniformLocation(program, name);
-		}
-
-		var programLocations = {
-			positionLocation: getAttrib("a_position"),
-			textureCoordLocation: getAttrib("a_textureCoord"),
-
-			projectionMatrixLocation: getUniform("u_projectionMatrix"),
-			viewMatrixLocation: getUniform("u_viewMatrix"),
-
-			boardRotationMatrixLocation: getUniform("u_boardRotationMatrix"),
-			boardTranslationMatrixLocation: getUniform("u_boardTranslationMatrix"),
-			selectorMatrixLocation: getUniform("u_selectorMatrix"),
-			ringMatrixLocation: getUniform("u_ringMatrix"),
-			cellMatrixLocation: getUniform("u_cellMatrix"),
-
-			yellowBoostLocation: getUniform("u_yellowBoost"),
-			alphaLocation: getUniform("u_alpha")
-		};
-
-		// Sets the canvas's width and height to the size it's being displayed at.
-		function resizeCanvas() {
-			// Compare current dimensions to displayed dimensions and set them if different.
-			if (canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
-				canvas.width = canvas.clientWidth;
-				canvas.height = canvas.clientHeight;
-				return true;
+			function getAttrib(name) {
+				return gl.getAttribLocation(program, name);
 			}
-			return false;
-		}
 
-		// Initially resize the canvas since setting the width to 100% width just scales.
-		resizeCanvas();
-
-		// Creates the projection matrix based upon the current canvas dimensions.
-		function makeProjectionMatrix() {
-			var aspect = canvas.width / canvas.height;
-			var fieldOfViewRadians = BC.Math.radians(90);
-			return BC.Matrix.makePerspective(fieldOfViewRadians, aspect, 1, 2000);
-		}
-
-		// Create the inital projection matrix.
-		var projectionMatrix = makeProjectionMatrix();
-
-		// Set a window resize event listener to adjust the canvas and projection matrix.
-		$(window).resize(function() {
-			if (resizeCanvas()) {
-				projectionMatrix = makeProjectionMatrix();
+			function getUniform(name) {
+				return gl.getUniformLocation(program, name);
 			}
-		});
 
-		// Creates the view matrix.
-		function makeViewMatrix() {
-			var cameraPosition = [0, 0.1, 3];
-			var targetPosition = [0, 0, 0];
-			var up = [0, 1, 0];
-			var cameraMatrix = BC.Matrix.makeLookAt(cameraPosition, targetPosition, up);
-			return BC.Matrix.makeInverse(cameraMatrix);
+			var programLocations = {
+				positionLocation: getAttrib("a_position"),
+				textureCoordLocation: getAttrib("a_textureCoord"),
+
+				projectionMatrixLocation: getUniform("u_projectionMatrix"),
+				viewMatrixLocation: getUniform("u_viewMatrix"),
+
+				boardRotationMatrixLocation: getUniform("u_boardRotationMatrix"),
+				boardTranslationMatrixLocation: getUniform("u_boardTranslationMatrix"),
+				selectorMatrixLocation: getUniform("u_selectorMatrix"),
+				ringMatrixLocation: getUniform("u_ringMatrix"),
+				cellMatrixLocation: getUniform("u_cellMatrix"),
+
+				yellowBoostLocation: getUniform("u_yellowBoost"),
+				alphaLocation: getUniform("u_alpha")
+			};
+
+			// Sets the canvas's width and height to the size it's being displayed at.
+			function resizeCanvas() {
+				// Compare current dimensions to displayed dimensions and set them if different.
+				if (canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
+					canvas.width = canvas.clientWidth;
+					canvas.height = canvas.clientHeight;
+					return true;
+				}
+				return false;
+			}
+
+			// Initially resize the canvas since setting the width to 100% width just scales.
+			resizeCanvas();
+
+			// Creates the projection matrix based upon the current canvas dimensions.
+			function makeProjectionMatrix() {
+				var aspect = canvas.width / canvas.height;
+				var fieldOfViewRadians = BC.Math.radians(90);
+				return BC.Matrix.makePerspective(fieldOfViewRadians, aspect, 1, 2000);
+			}
+
+			// Create the inital projection matrix.
+			var projectionMatrix = makeProjectionMatrix();
+
+			// Set a window resize event listener to adjust the canvas and projection matrix.
+			$(window).resize(function() {
+				if (resizeCanvas()) {
+					projectionMatrix = makeProjectionMatrix();
+				}
+			});
+
+			// Creates the view matrix.
+			function makeViewMatrix() {
+				var cameraPosition = [0, 0.1, 3];
+				var targetPosition = [0, 0, 0];
+				var up = [0, 1, 0];
+				var cameraMatrix = BC.Matrix.makeLookAt(cameraPosition, targetPosition, up);
+				return BC.Matrix.makeInverse(cameraMatrix);
+			}
+
+			// Create the initial view matrix. Doesn't need to be set again later.
+			var viewMatrix = makeViewMatrix();
+			gl.uniformMatrix4fv(programLocations.viewMatrixLocation, false, viewMatrix);
+
+			var metrics = {
+				numRings: 3,
+				numCells: 24,
+				numBlockTypes: 6,
+				ringInnerRadius: 0.75,
+				ringOuterRadius: 1,
+				ringHeight: 0.3
+			};
+
+			var resources = BC.Resources.make();
+
+			var board = BC.Board.make(metrics);
+			var boardView = BC.Board.View.make({
+				board: board,
+				gl: gl,
+				programLocations: programLocations,
+				resources: resources
+			});
+
+			var controller = BC.Controller.make(canvas);
+			controller.setMoveLeftListener(function() {
+				board.move(Direction.LEFT);
+			});
+			controller.setMoveRightListener(function() {
+				board.move(Direction.RIGHT);
+			});
+			controller.setMoveUpListener(function() {
+				board.move(Direction.UP);
+			});
+			controller.setMoveDownListener(function() {
+				board.move(Direction.DOWN);
+			});
+			controller.setPrimaryActionListener(function() {
+				board.swap();
+			});
+
+			var watch = BC.StopWatch.make();
+
+			function drawScene() {
+				gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+				gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+				gl.uniformMatrix4fv(programLocations.projectionMatrixLocation, false, projectionMatrix);
+
+				watch.tick();
+
+				board.update(watch);
+				boardView.draw();
+
+				requestAnimationFrame(drawScene);
+			}
+
+			drawScene();
 		}
-
-		// Create the initial view matrix. Doesn't need to be set again later.
-		var viewMatrix = makeViewMatrix();
-		gl.uniformMatrix4fv(programLocations.viewMatrixLocation, false, viewMatrix);
-
-		var metrics = {
-			numRings: 3,
-			numCells: 24,
-			numBlockTypes: 6,
-			ringInnerRadius: 0.75,
-			ringOuterRadius: 1,
-			ringHeight: 0.3
-		};
-
-		var resources = BC.Resources.make();
-
-		var board = BC.Board.make(metrics);
-		var boardView = BC.Board.View.make({
-			board: board,
-			gl: gl,
-			programLocations: programLocations,
-			resources: resources
-		});
-
-		var controller = BC.Controller.make(canvas);
-		controller.setMoveLeftListener(function() {
-			board.move(Direction.LEFT);
-		});
-		controller.setMoveRightListener(function() {
-			board.move(Direction.RIGHT);
-		});
-		controller.setMoveUpListener(function() {
-			board.move(Direction.UP);
-		});
-		controller.setMoveDownListener(function() {
-			board.move(Direction.DOWN);
-		});
-		controller.setPrimaryActionListener(function() {
-			board.swap();
-		});
-
-		var watch = BC.StopWatch.make();
-
-		function drawScene() {
-			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-			gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-			gl.uniformMatrix4fv(programLocations.projectionMatrixLocation, false, projectionMatrix);
-
-			watch.tick();
-
-			board.update(watch);
-			boardView.draw();
-
-			requestAnimationFrame(drawScene);
-		}
-
-		drawScene();
 	}
 
 	return parent;
