aed55cd45df76a5c66fda6db569d03bb9629c989
Brian Muramatsu <btmura@gmail.com>
2014-05-24 00:57:14 -0700

    another attempt to improve the sounds

--- src/res/js/bc/audio/sound.js
+++ src/res/js/bc/audio/sound.js
@@ -2,21 +2,18 @@ var BC = (function(root) {
 
 	var me = root.Audio = root.Audio || {};
 
-	// Sounds are spaced 1 sec apart in the single file.
-	var DURATION = 0.5;
-
-	function newSound(offset) {
+	function newSound(offset, duration) {
 		return {
 			offset: offset,
-			duration: DURATION
+			duration: duration
 		};
 	}
 
 	me.Sound = {
-		BUTTON_CLICK: newSound(0),
-		SELECTOR_MOVEMENT: newSound(1),
-		CELL_SWAP: newSound(2),
-		CELL_CLEAR: newSound(3)
+		BUTTON_CLICK: newSound(0, 0.75),
+		SELECTOR_MOVEMENT: newSound(1, 0.5),
+		CELL_SWAP: newSound(2, 0.5),
+		CELL_CLEAR: newSound(3, 0.5)
 	};
 
 	return root;
