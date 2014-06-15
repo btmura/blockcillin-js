57433e3b49c28694adaf31960acd9a6f048b37da
Brian Muramatsu <btmura@gmail.com>
2014-06-14 22:51:17 -0700

    render stats properly in the menu

--- src/res/js/bc/app/stat.js
+++ src/res/js/bc/app/stat.js
@@ -4,8 +4,11 @@ var BC = (function(root) {
 	var me = parent.Stat = parent.Stat || {};
 
 	me.make = function(args) {
-		var value = args.value;
-		var unit = args.unit;
+		args = args || {};
+
+		var value = args.value || 0;
+		var unit = args.unit || BC.Common.Unit.NONE;
+
 		return {
 			value: value,
 			unit: unit
--- src/res/js/bc/menu/stats.js
+++ src/res/js/bc/menu/stats.js
@@ -5,23 +5,48 @@ var BC = (function(root) {
 
 	me.make = function(args) {
 		var MENU_FADE_SPEED = "slow";
+		var MAX_STATS = 5;
+
+		var Stat = BC.App.Stat;
+		var Unit = BC.Common.Unit;
 
 		var statBoard = args.statBoard;
 
 		var menu = $("#stats-menu");
 		var noStatsLabel = $("#no-stats-label", menu);
-		var entries = [
-			$("#stats-menu-entry-1", menu),
-			$("#stats-menu-entry-2", menu),
-			$("#stats-menu-entry-3", menu),
-			$("#stats-menu-entry-4", menu),
-			$("#stats-menu-entry-5", menu)
-		];
 		var closeButton = $("#close-button", menu);
+
+		var entries = [];
+		var views = [];
+		for (var i = 1; i <= MAX_STATS; i++) {
+			var entry = $("#stats-menu-entry-" + i, menu);
+			entries.push(entry);
+
+			function makeView(id) {
+				return BC.View.Stat.make($(id, entry));
+			}
+
+			views.push({
+				speedLevel: makeView("#speed-level-stat"),
+				elapsedTime: makeView("#elapsed-time-stat"),
+				score: makeView("#score-stat")
+			});
+		}
+
+		var models = {
+			speedLevel: Stat.make(),
+			elapsedTime: Stat.make({unit: Unit.SECONDS}),
+			score: Stat.make()
+		};
 
 		closeButton.click(function() {
 			hide();
 		});
+
+		function show() {
+			refresh();
+			menu.fadeIn(MENU_FADE_SPEED);
+		}
 
 		function refresh() {
 			var stats = statBoard.getStats();
@@ -34,25 +59,19 @@ var BC = (function(root) {
 
 			for (var i = 0; i < entries.length; i++) {
 				if (i < stats.length) {
-					var speedLevelStat = $("#speed-level-stat", entries[i]);
-					speedLevelStat.text(stats[i].speedLevel);
+					models.speedLevel.value = stats[i].speedLevel;
+					models.elapsedTime.value = stats[i].elapsedTime;
+					models.score.value = stats[i].score;
 
-					var elapsedTimeStat = $("#elapsed-time-stat", entries[i]);
-					elapsedTimeStat.text(stats[i].elapsedTime);
-
-					var scoreStat = $("#score-stat", entries[i]);
-					scoreStat.text(stats[i].score);
+					views[i].speedLevel.draw(models.speedLevel);
+					views[i].elapsedTime.draw(models.elapsedTime);
+					views[i].score.draw(models.score);
 
 					entries[i].show();
 				} else {
 					entries[i].hide();
 				}
 			}
-		}
-
-		function show() {
-			refresh();
-			menu.fadeIn(MENU_FADE_SPEED);
 		}
 
 		function hide() {
