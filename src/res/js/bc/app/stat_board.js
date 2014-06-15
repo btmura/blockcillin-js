84c45429769a5269ce7723d933896b5fefe51189
Brian Muramatsu <btmura@gmail.com>
2014-06-14 22:05:44 -0700

    rank top scores

--- src/res/js/bc/app/stat_board.js
+++ src/res/js/bc/app/stat_board.js
@@ -5,14 +5,9 @@ var BC = (function(root) {
 
 	me.make = function(args) {
 		var STORAGE_KEY = "bc.stats";
+		var MAX_STATS = 5;
 
 		var storage = args.storage;
-
-		function addStats(newStats) {
-			var stats = getStats();
-			stats.push(newStats);
-			storage.set(STORAGE_KEY, JSON.stringify(stats));
-		}
 
 		function getStats() {
 			var json = storage.get(STORAGE_KEY);
@@ -22,9 +17,31 @@ var BC = (function(root) {
 			return [];
 		}
 
+		function addStats(newStats) {
+			var stats = getStats();
+			stats.push(newStats);
+			stats.sort(comparator);
+			stats.slice(0, MAX_STATS);
+			storage.set(STORAGE_KEY, JSON.stringify(stats));
+		}
+
+		function comparator(s1, s2) {
+			var diff = s2.score - s1.score;
+			if (diff !== 0) {
+				return diff;
+			}
+
+			var diff = s2.elapsedTime - s1.elapsedTime;
+			if (diff !== 0) {
+				return diff;
+			}
+
+			return s2.speedLevel - s1.speedLevel;
+		}
+
 		return {
-			addStats: addStats,
-			getStats: getStats
+			getStats: getStats,
+			addStats: addStats
 		};
 	};
 
